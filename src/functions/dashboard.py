4a66455a30695ef63f384406113520d0f7471aee
Timothe Fadenipo <timothe.fadenipo@etudiant.univ-reims.fr>
2025-10-17 15:08:51 +0200

    change path to find the database

--- src/functions/dashboard.py
+++ src/functions/dashboard.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from pathlib import Path
 
 
 def main_streamlit():
-    # On lit la base (chemin à changer pour que tout le monde puisse charger la base de données)
-    data = pd.read_csv(
-        "C:/Users/arvoi/Desktop/cours/management_digit/data/Motor vehicle insurance data.csv",
-        sep=";",
-    )
-
+    # Chemin relatif basé sur ce fichier
+    data_path = Path(__file__).resolve().parents[2] / "data/raw/Motor_vehicle_insurance_data.csv"
+    
+    # Lecture de la base
+    data = pd.read_csv(data_path, sep=";")
+    
     st.title("Analyse des premières variables")
 
     choix = st.selectbox("Choisis une variable :", data.columns)
