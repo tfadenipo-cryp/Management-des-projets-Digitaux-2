c968b8633f080a4b235b56719b59268793a615b8
idrissjourdan <idrissjourdan@MacBook-Pro-de-Idriss.local>
2025-10-26 00:36:21 +0200

    changed loaddata

--- src/functions/load_data.py
+++ src/functions/load_data.py
@@ -9,12 +9,6 @@ def load_data() -> pd.DataFrame:
     Load the processed vehicle insurance dataset.
     Automatically detects the correct path and standardizes column names.
     """
-<<<<<<< HEAD
-    
-    #old way to load the data
-=======
-
->>>>>>> 05bb5a24fe5747daad24eb91f7a3099066c6a828
     project_root = Path(__file__).resolve()
     while not (project_root / "data" / "processed").exists() and project_root != project_root.parent:
         project_root = project_root.parent
@@ -22,12 +16,8 @@ def load_data() -> pd.DataFrame:
     csv_path = project_root / "data" / "processed" / "new_motor_vehicle_insurance_data.csv"
 
     try:
-<<<<<<< HEAD
-        df = pd.read_csv(csv_path, sep=",")
-=======
         # ✅ Back to semicolon separator
         df = pd.read_csv(csv_path, sep=";")
->>>>>>> 05bb5a24fe5747daad24eb91f7a3099066c6a828
     except FileNotFoundError:
         st.error(f"❌ File not found: {csv_path}")
         return pd.DataFrame()
@@ -43,22 +33,6 @@ def load_data() -> pd.DataFrame:
         .str.replace(r"[^a-z0-9]+", "_", regex=True)
     )
 
-<<<<<<< HEAD
-
-
-    #other method to loads the data with the new file engineering (we do not have to download the data now
-    # and if the wesite wants to add new row to their data, the project will follow the changes)
-    data = engineering()
-    
-    data.columns = (
-        data.columns
-        .str.strip()
-        .str.lower()
-        .str.replace(r"[^a-z0-9]+", "_", regex=True)
-    )
-    
-    return data
-=======
     # ✅ Fix plural or inconsistent names
     rename_map = {
         "claim_costs_year": "cost_claims_year",  # normalize
@@ -66,4 +40,3 @@ def load_data() -> pd.DataFrame:
     df.rename(columns=rename_map, inplace=True)
 
     return df
->>>>>>> 05bb5a24fe5747daad24eb91f7a3099066c6a828
