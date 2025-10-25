88bcb3f3e782871d446f11e0b678a21c0542936e
matthisarvois <arvois.matthis60@gmail.com>
2025-10-25 12:14:34 +0200

    file take from website and variable type_fuel transformed

--- src/functions/load_data.py
+++ src/functions/load_data.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pandas as pd
 import streamlit as st
+from engineering import engineering
 
 
 @st.cache_data
@@ -9,6 +10,8 @@ def load_data() -> pd.DataFrame:
     Load the processed vehicle insurance dataset.
     The function automatically detects the correct path for any environment.
     """
+    
+    #old way to load the data
     project_root = Path(__file__).resolve()
     while not (project_root / "data" / "processed").exists() and project_root != project_root.parent:
         project_root = project_root.parent
@@ -16,7 +19,7 @@ def load_data() -> pd.DataFrame:
     csv_path = project_root / "data" / "processed" / "new_motor_vehicle_insurance_data.csv"
 
     try:
-        df = pd.read_csv(csv_path, sep=";")
+        df = pd.read_csv(csv_path, sep=",")
     except FileNotFoundError:
         st.error(f"❌ File not found: {csv_path}")
         return pd.DataFrame()
@@ -31,4 +34,17 @@ def load_data() -> pd.DataFrame:
         .str.replace(r"[^a-z0-9]+", "_", regex=True)
     )
 
-    return df
+
+
+    #other method to loads the data with the new file engineering (we do not have to download the data now
+    # and if the wesite wants to add new row to their data, the project will follow the changes)
+    data = engineering()
+    
+    data.columns = (
+        data.columns
+        .str.strip()
+        .str.lower()
+        .str.replace(r"[^a-z0-9]+", "_", regex=True)
+    )
+    
+    return data
